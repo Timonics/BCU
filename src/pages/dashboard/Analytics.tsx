19eff84f33df69818040a88fdb039af800801535
Oderinde Michael <Olubiyioderinde7@gmail.com>
2025-03-27 08:31:07 +0100

    refactor dashboard; extract Analytics and PendingApprovals components

---
+++ src/pages/dashboard/Analytics.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Analytics: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center gap-2 pt-4">
+      <div className="flex w-full gap-2">
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+      </div>
+      <div className="flex w-full gap-2">
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+        <div className="w-1/4 bg-[#F9FAFB] border-[1.42px] flex flex-col gap-2 border-black/30 p-4 rounded-xl">
+          <p className="text-xs text-black/75">Total Members</p>
+          <p className="text-black/85 pops font-bold">6451</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
